4687eb0cef91c1cbcd3d2bc4b63ef2ad4936ab2d
Sagar Mainali <sagarmainali78@gmail.com>
2024-08-12 15:00:43 +0545

    final update

--- src/Components/Profiles.tsx
+++ src/Components/Profiles.tsx
@@ -6,42 +6,45 @@ export default function Profiles({ users }: PropsType) {
         <div>
 
             <h2 className='sub-title'>All user data</h2>
-
-            <table className='w-full mb-4'>
-                <thead>
-                    <tr>
-                        <th>Username</th>
-                        <th>Email</th>
-                        <th>Phone</th>
-                        <th>DOB</th>
-                        <th>City</th>
-                        <th>District</th>
-                        <th>Province</th>
-                        <th>Country</th>
-                    </tr>
-                </thead>
-
-                <tbody>
-
-                    {
-                        users.map((user: UserType) => {
-                            const entries = Object.entries(user).filter(([key]) => key !== 'id' && key !== 'profilePic')
-
-                            return <tr key={user.id}>
-                                {
-                                    entries.map(([key, value]) => (
-                                        (
-                                            <td key={key}>{value}</td>
-                                        )
-                                    )
-                                    )
-                                }
+            {
+                users.length < 1
+                    ? <h4 className="my-2 text-sm">'No user data at the moment'</h4>
+                    : <table className='w-full mb-4'>
+                        <thead>
+                            <tr>
+                                <th>Username</th>
+                                <th>Email</th>
+                                <th>Phone</th>
+                                <th>DOB</th>
+                                <th>City</th>
+                                <th>District</th>
+                                <th>Province</th>
+                                <th>Country</th>
                             </tr>
-                        }
-                        )
-                    }
-                </tbody>
-            </table>
+                        </thead>
+
+                        <tbody>
+
+                            {
+                                users.map((user: UserType) => {
+                                    const entries = Object.entries(user).filter(([key]) => key !== 'id' && key !== 'profilePic')
+
+                                    return <tr key={user.id}>
+                                        {
+                                            entries.map(([key, value]) => (
+                                                (
+                                                    <td key={key}>{value}</td>
+                                                )
+                                            )
+                                            )
+                                        }
+                                    </tr>
+                                }
+                                )
+                            }
+                        </tbody>
+                    </table>
+            }
 
             <Link to="/" className="text-blue-500">Go to Home Page</Link>
 
--- src/Components/Table.tsx
+++ src/Components/Table.tsx
@@ -56,67 +56,70 @@ export default function Table({ users, setUsers }: PropsType) {
   return (
     <div>
       <h2 className='sub-title'>User Data</h2>
-      <table className='w-full'>
-        <thead>
-          <tr>
-            <th>Username</th>
-            <th>Email</th>
-            <th>Phone</th>
-            <th>DOB</th>
-            <th>City</th>
-            <th>District</th>
-            <th>Province</th>
-            <th>Country</th>
-            <th>Action</th>
-          </tr>
-        </thead>
-
-        <tbody>
-          {
-            currentUsers.map((user: UserType) => (
-              <tr key={user.id}>
-                {
-                  Object.entries(user).filter(([key]) => key !== 'id' && key !== 'profilePic').map(([key, value]) => (
-                    editId === user.id ? (
-                      <td key={key}>
-                        <input
-                          type="text"
-                          name={key}
-                          value={(editData as any)[key]}
-                          onChange={handleChange}
-                        />
-                      </td>
-                    ) : (
-                      <td key={key}>{value}</td>
-                    )
-                  ))
-                }
-                <td>
-                  {
-                    editId === user.id ? (
-                      <>
-                        <span className="text-green-500 text-xs mr-4" onClick={() => handleUpdate(user.id)}>Save</span>
-                        <span className="text-gray-500 text-xs" onClick={() => setEditId(null)}>Cancel</span>
-                      </>
-                    ) : (
-                      <>
-                        <span className="text-blue-500 text-xs mr-4" onClick={() => handleEdit(user)}>Edit</span>
-                        <span className="text-red-500 text-xs" onClick={() => deleteUserData(user.id)}>Delete</span>
-                      </>
-                    )
-                  }
-                </td>
+      {
+        users.length < 1
+          ? <h4 className="my-2 text-sm">'No user data at the moment'</h4>
+          : <table className='w-full'>
+            <thead>
+              <tr>
+                <th>Username</th>
+                <th>Email</th>
+                <th>Phone</th>
+                <th>DOB</th>
+                <th>City</th>
+                <th>District</th>
+                <th>Province</th>
+                <th>Country</th>
+                <th>Action</th>
               </tr>
-            ))
-          }
-        </tbody>
-      </table>
+            </thead>
+            <tbody>
+              {
+                currentUsers.map((user: UserType) => (
+                  <tr key={user.id}>
+                    {
+                      Object.entries(user).filter(([key]) => key !== 'id' && key !== 'profilePic').map(([key, value]) => (
+                        editId === user.id ? (
+                          <td key={key}>
+                            <input
+                              type="text"
+                              name={key}
+                              value={(editData as any)[key]}
+                              onChange={handleChange}
+                            />
+                          </td>
+                        ) : (
+                          <td key={key}>{value}</td>
+                        )
+                      ))
+                    }
+                    <td>
+                      {
+                        editId === user.id ? (
+                          <>
+                            <span className="text-green-500 text-xs mr-4" onClick={() => handleUpdate(user.id)}>Save</span>
+                            <span className="text-gray-500 text-xs" onClick={() => setEditId(null)}>Cancel</span>
+                          </>
+                        ) : (
+                          <>
+                            <span className="text-blue-500 text-xs mr-4" onClick={() => handleEdit(user)}>Edit</span>
+                            <span className="text-red-500 text-xs" onClick={() => deleteUserData(user.id)}>Delete</span>
+                          </>
+                        )
+                      }
+                    </td>
+                  </tr>
+                ))
+              }
+            </tbody>
+          </table>
+      }
 
       {/* pagination */}
       <div className='my-4'>
         <button className='disabled:opacity-30'
           onClick={() => handlePageChange(currentPage - 1)}
-          disabled={currentPage === 1}
+          disabled={currentPage === 1 || totalPages === 0}
         >
           Previous
         </button>
@@ -131,7 +134,7 @@ export default function Table({ users, setUsers }: PropsType) {
         ))}
         <button className='disabled:opacity-30'
           onClick={() => handlePageChange(currentPage + 1)}
-          disabled={currentPage === totalPages}
+          disabled={currentPage === totalPages  || totalPages === 0}
         >
           Next
         </button>
